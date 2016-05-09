05c23278d0a71b53239d4dfd26b5f521a94d1762
Vui Nguyen <vui_engineer@yahoo.com>
2016-05-09 11:17:41 -0600

    Stuff written during technical interview

--- TrackName/ViewController.swift
+++ TrackName/ViewController.swift
@@ -8,11 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    //var cellContent = ["Vui", "Eric", "Boulder", "Senna", "P-Dude"]
+    var cellContent:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = NSURL(string:"https://itunes.apple.com/search?term=u2")!
+        
+        let session = NSURLSession.sharedSession()
+        
+        
+        
+        let task = session.dataTaskWithURL(url) { (data , response, error ) -> Void in
+            if error != nil {
+                print(error)
+            }
+            else {
+                if let data = data {
+                   // print(NSString(data: data, encoding: NSUTF8StringEncoding))
+                    
+                    
+                    
+                    do {
+                        let jsonResult = try NSJSONSerialization.JSONObjectWithData(data , options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                        //print(jsonResult)
+                        
+                        if jsonResult.count > 0 {
+                            
+                            if let items = jsonResult["results"] as? NSArray {
+                                
+                                for result in items {
+                                    let trackName: String = (result["trackName"] as? String)!
+                                        print(trackName)
+                                    self.cellContent.append(trackName)
+                                    
+                                    dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+                                        self.tableView.reloadData()
+                                    }
+                                    
+                              //self.tableView.reloadData()
+                                }
+                                
+                            }
+                        }
+
+                    }
+                    catch {
+                        print("error reading JSON data")
+                    }
+
+                }
+            }
+        }
+        task.resume()
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +81,19 @@ class ViewController: UIViewController {
     }
 
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
